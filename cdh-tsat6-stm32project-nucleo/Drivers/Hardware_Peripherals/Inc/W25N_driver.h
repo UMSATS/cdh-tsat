4746ac861669025d22be4ef63784019bcdbf4dd3
DaighB-umsats <daigh.burgess@umsats.ca>
2022-11-13 17:58:25 -0600

    Add W25N Prefix to Defines

--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/W25N_driver.h
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/W25N_driver.h
@@ -27,23 +27,23 @@
 #define W25N_nHOLD_GPIO   GPIOC
 #define W25N_nHOLD_PIN    GPIO_PIN_7
 
-#define W25N_DUMMY_BYTE                    0x00
+#define W25N_DUMMY_BYTE                         0x00
 
-#define OPCODE_DEVICE_RESET                0xFF
-#define OPCODE_READ_JEDEC_ID               0x9F
-#define OPCODE_READ_STATUS_REGISTER        0x0F
-#define OPCODE_WRITE_STATUS_REGISTER       0x1F
-#define OPCODE_WRITE_ENABLE                0x06
-#define OPCODE_WRITE_DISABLE               0x04
-#define OPCODE_BAD_BLOCK_MANAGEMENT        0xA1
-#define OPCODE_READ_BBM_LUT                0xA5
-#define OPCODE_BLOCK_ERASE_128KB           0xD8
-#define OPCODE_LOAD_PROGRAM_DATA           0x02
-#define OPCODE_PROGRAM_EXECUTE             0x10
-#define OPCODE_PAGE_DATA_READ              0x13
-#define OPCODE_READ_DATA                   0x03
+#define W25N_OPCODE_DEVICE_RESET                0xFF
+#define W25N_OPCODE_READ_JEDEC_ID               0x9F
+#define W25N_OPCODE_READ_STATUS_REGISTER        0x0F
+#define W25N_OPCODE_WRITE_STATUS_REGISTER       0x1F
+#define W25N_OPCODE_WRITE_ENABLE                0x06
+#define W25N_OPCODE_WRITE_DISABLE               0x04
+#define W25N_OPCODE_BAD_BLOCK_MANAGEMENT        0xA1
+#define W25N_OPCODE_READ_BBM_LUT                0xA5
+#define W25N_OPCODE_BLOCK_ERASE_128KB           0xD8
+#define W25N_OPCODE_LOAD_PROGRAM_DATA           0x02
+#define W25N_OPCODE_PROGRAM_EXECUTE             0x10
+#define W25N_OPCODE_PAGE_DATA_READ              0x13
+#define W25N_OPCODE_READ_DATA                   0x03
 
-#define BBM_LUT_NUM_OF_BYTES    80 //(4 bytes per entry) * (20 entries) = 80 bytes
+#define W25N_BBM_LUT_NUM_OF_BYTES    80 //(4 bytes per entry) * (20 entries) = 80 bytes
 
 //###############################################################################################
 //Global Variable Declarations
--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/W25N_driver.c
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/W25N_driver.c
@@ -21,7 +21,7 @@
 //###############################################################################################
 void W25N_Bad_Block_Management(uint16_t logical_block_address, uint16_t physical_block_address)
 {
-    uint8_t opcode = OPCODE_BAD_BLOCK_MANAGEMENT;
+    uint8_t opcode = W25N_OPCODE_BAD_BLOCK_MANAGEMENT;
 
     HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
 
@@ -34,7 +34,7 @@ void W25N_Bad_Block_Management(uint16_t logical_block_address, uint16_t physical
 
 void W25N_Read_BBM_LUT(uint8_t *p_buffer)
 {
-    uint8_t opcode = OPCODE_READ_BBM_LUT;
+    uint8_t opcode = W25N_OPCODE_READ_BBM_LUT;
     uint8_t dummy_byte = W25N_DUMMY_BYTE;
 
     HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
@@ -42,7 +42,7 @@ void W25N_Read_BBM_LUT(uint8_t *p_buffer)
     HAL_SPI_Transmit(&W25N_SPI, &opcode, 1, HAL_MAX_DELAY);
     HAL_SPI_Transmit(&W25N_SPI, &dummy_byte, 1, HAL_MAX_DELAY);
 
-    HAL_SPI_Receive(&W25N_SPI, p_buffer, BBM_LUT_NUM_OF_BYTES, HAL_MAX_DELAY);
+    HAL_SPI_Receive(&W25N_SPI, p_buffer, W25N_BBM_LUT_NUM_OF_BYTES, HAL_MAX_DELAY);
 
     HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_SET);
 }
@@ -51,7 +51,7 @@ void W25N_Read_BBM_LUT(uint8_t *p_buffer)
 //OR BLOCK ADDRESS (DATASHEET SAYS PAGE ADDRESS)
 void W25N_Block_Erase_128KB(uint16_t page_address)
 {
-    uint8_t opcode = OPCODE_BLOCK_ERASE_128KB;
+    uint8_t opcode = W25N_OPCODE_BLOCK_ERASE_128KB;
     uint8_t dummy_byte = W25N_DUMMY_BYTE;
 
     HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
@@ -65,7 +65,7 @@ void W25N_Block_Erase_128KB(uint16_t page_address)
 
 void W25N_Load_Program_Data(uint8_t *p_buffer, uint16_t column_address, uint16_t num_of_bytes)
 {
-    uint8_t opcode = OPCODE_LOAD_PROGRAM_DATA;
+    uint8_t opcode = W25N_OPCODE_LOAD_PROGRAM_DATA;
 
     HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
 
@@ -78,7 +78,7 @@ void W25N_Load_Program_Data(uint8_t *p_buffer, uint16_t column_address, uint16_t
 
 void W25N_Program_Execute(uint16_t page_address)
 {
-    uint8_t opcode = OPCODE_PROGRAM_EXECUTE;
+    uint8_t opcode = W25N_OPCODE_PROGRAM_EXECUTE;
     uint8_t dummy_byte = W25N_DUMMY_BYTE;
 
     HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
@@ -92,7 +92,7 @@ void W25N_Program_Execute(uint16_t page_address)
 
 void W25N_Page_Data_Read(uint16_t page_address)
 {
-    uint8_t opcode = OPCODE_PAGE_DATA_READ;
+    uint8_t opcode = W25N_OPCODE_PAGE_DATA_READ;
     uint8_t dummy_byte = W25N_DUMMY_BYTE;
 
     HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
@@ -106,7 +106,7 @@ void W25N_Page_Data_Read(uint16_t page_address)
 
 void W25N_Read_Data(uint8_t *p_buffer, uint16_t column_address, uint16_t num_of_bytes)
 {
-    uint8_t opcode = OPCODE_READ_DATA;
+    uint8_t opcode = W25N_OPCODE_READ_DATA;
     uint8_t dummy_byte = W25N_DUMMY_BYTE;
 
     HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
