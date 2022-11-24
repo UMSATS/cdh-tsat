1f16370c25d1dd2abf861f13bade9c5827d35389
Rodrigo-AL <79809345+Rodrigo-AL@users.noreply.github.com>
2022-11-23 19:24:16 -0600

    Update W25N_driver.c
    First 07 functions written.

--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/W25N_driver.c
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/W25N_driver.c
@@ -19,6 +19,76 @@
 //###############################################################################################
 //Driver Functions
 //###############################################################################################
+void W25N_Device_Reset(){
+	uint8_t opcode = W25N_OPCODE_DEVICE_RESET;
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
+	HAL_SPI_Transmit(&W25N,&opcode,1,W25N_SPI_DELAY);
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_SET);
+}
+
+void W25N_Read_JEDEC_ID(uint8_t *p_buffer,){
+	uint8_t opcode = W25N_OPCODE_READ_JEDEC_ID;
+	uint8_t dummy_byte = W25N_DUMMY_BYTE;
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
+
+	HAL_SPI_Transmit(&W25N_SPI, &opcode, 1, W25N_SPI_DELAY);
+	HAL_SPI_Transmit(&W25N_SPI,&dummy_byte, 1, W25N_SPI_DELAY);
+
+	HAL_SPI_Receive(&W25N_SPI, p_buffer,3, W25N_SPI_DELAY);
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_SET);
+
+}
+
+void W25N_Read_Status_Register(uint8_t register_address, uint8_t *p_buffer){
+	uint8_t opcode = W25N_OPCODE_READ_STATUS_REGISTER;
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
+
+	HAL_SPI_Transmit(&W25N_SPI,&opcode,1,W25N_SPI_DELAY);
+	HAL_SPI_Transmit(&W25N_SPI,&register_address,1, W25N_SPI_DELAY);
+
+	HAL_SPI_Receive(&W25N_SPI,p_buffer,1,W25N_SPI_DELAY);
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_SET);
+}
+
+void W25N_Write_Status_Register(uint8_t register_address, uint8_t register_value, uint8_t *p_buffer){
+	uint8_t opcode = W25N_OPCODE_READ_STATUS_REGISTER;
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
+
+	HAL_SPI_Transmit(&W25N_SPI, &opcode, 1, W25N_SPI_DELAY);
+
+	W25N_SPI_Transmit(&W25N_SPI, &register_address,1,W25N_SPI_DELAY);
+	W25N_SPI_Transmit(&W25N_SPI, &register_value,1,W25N_SPI_DELAY);
+
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_SET);
+}
+
+void W25N_Write_Enable(){
+	uint8_t opcode = W25N_OPCODE_WRITE_ENABLE;
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
+	HAL_SPI_Transmit(&W25N,&opcode,1,W25N_SPI_DELAY);
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_SET);
+}
+
+void W25N_Write_Disable(){
+	uint8_t opcode = W25N_OPCODE_WRITE_DISABLE;
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_RESET);
+	HAL_SPI_Transmit(&W25N,&opcode,1,W25N_SPI_DELAY);
+
+	HAL_GPIO_WritePin(W25N_nCS_GPIO, W25N_nCS_PIN, GPIO_PIN_SET);
+}
+
+
 void W25N_Bad_Block_Management(uint16_t logical_block_address, uint16_t physical_block_address)
 {
     uint8_t opcode = W25N_OPCODE_BAD_BLOCK_MANAGEMENT;
