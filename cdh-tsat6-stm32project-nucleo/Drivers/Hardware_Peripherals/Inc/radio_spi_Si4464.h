65e774921b3fbb25db289bf3c8f1fd16484f94ac
Nikolaus J. Reichert <nikolaus.reichert@umsats.ca>
2023-01-11 18:04:46 -0600

    Move comments to header and add Reset function boilerplate.

--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/radio_spi_Si4464.h
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/radio_spi_Si4464.h
@@ -18,17 +18,63 @@ extern SPI_HandleTypeDef hspi2;
  * Using the SPI in an interrupt mode means we must setup callback functions
  * for the TX and RX. The functions below are the Transmit, Receive and Transmit Receive Respectively
  */
+
+/**
+ * @brief Transmit Callback Function
+ */
 void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef * hspi2);
 
+/**
+ * @brief Receive Callback Function
+ */
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef * hspi2);
 
+/**
+ * @brief Transmit/Receive Callback Function
+ */
 void HAL_SPI_TxRxCpltCallback (SPI_HandleTypeDef * hspi2);
 
 // Transmit and Receive Functions
+
+/**
+ * @brief Send data to the Radio.
+ * Effectively a wrapper for HAL_SPI_Transmit().
+ *
+ * @param pData A pointer to the data to send.
+ * @param numToSend The number of bytes to send.
+ *
+ * @returns the status code returned by the HAL.
+ */
 HAL_StatusTypeDef Radio_SPI_Transmit_Message(uint8_t * pData, size_t numToSend);
 
+/**
+ * @brief Receive up to numToReceive bytes of data from the Radio.
+ * Effecvively a wrapper for HAL_SPI_Receive().
+ *
+ * @param pData A pointer to where to receive data.
+ * @param numToReceive The maximum number of bytes to receive.
+ *
+ * @returns the status code returned by the HAL.
+ */
 HAL_StatusTypeDef Radio_SPI_Receive_Message(uint8_t * pData, size_t numToReceive);
 
+/**
+ * @brief Transmit/Receive numTransmittedReceived bytes to/from the Radio.
+ * Effectively a wrapper for HAL_SPI_TransmitReceive().
+ *
+ * @param pTxData A pointer to the data to send.
+ * @param pRxData A pointer to where to receive data.
+ * @prarm numTransmittedReceived The number of bytes to handle.
+ *
+ * @returns the status code returned by the HAL.
+ */
 HAL_StatusTypeDef Radio_SPI_Transmit_Receive_Message(uint8_t * pTxData, uint8_t * pRxData, size_t numTransmittedReceived);
+
+/**
+ * @brief Reset the Radio via its SDN Pin.
+ *
+ * @returns the status code returned by the HAL.
+ */
+HAL_StatusTypeDef Si4464_Reset_Device();
 
 #endif /* INC_SI446X_RADIO_SPI_SI4464_H_ */
--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/radio_spi_Si4464.c
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/radio_spi_Si4464.c
@@ -10,15 +10,6 @@
 #include "radio_spi_Si4464.h"
 
 
-/**
- * @brief Send data to the Radio.
- * Effectively a wrapper for HAL_SPI_Transmit().
- *
- * @param pData A pointer to the data to send.
- * @param numToSend The number of bytes to send.
- *
- * @returns the status code returned by the HAL.
- */
 HAL_StatusTypeDef Radio_SPI_Transmit_Message(uint8_t * pData, size_t numToSend){
 	// TODO: Check which form of SPI Transfer function we should use: Blocking, IT, or DMA.
 	// For now, Daigh noted we should use Blocking. -NJR
@@ -26,58 +17,44 @@ HAL_StatusTypeDef Radio_SPI_Transmit_Message(uint8_t * pData, size_t numToSend){
 	return HAL_SPI_Transmit(&hspi2, pData, numToSend, HAL_MAX_DELAY);
 }
 
-/**
- * @brief Receive up to numToReceive bytes of data from the Radio.
- * Effecvively a wrapper for HAL_SPI_Receive().
- *
- * @param pData A pointer to where to receive data.
- * @param numToReceive The maximum number of bytes to receive.
- *
- * @returns the status code returned by the HAL.
- */
 HAL_StatusTypeDef Radio_SPI_Receive_Message(uint8_t * pData, size_t numToReceive){
 	// See note on Radio_SPI_Transmit_Message(). -NJR
 	// HAL_SPI_Receive_IT(&hspi2, pData, sizeof(pData));
 	return HAL_SPI_Receive(&hspi2, pData, numToReceive, HAL_MAX_DELAY);
 }
 
-/**
- * @brief Transmit/Receive numTransmittedReceived bytes to/from the Radio.
- * Effectively a wrapper for HAL_SPI_TransmitReceive().
- *
- * @param pTxData A pointer to the data to send.
- * @param pRxData A pointer to where to receive data.
- * @prarm numTransmittedReceived The number of bytes to handle.
- *
- * @returns the status code returned by the HAL.
- */
 HAL_StatusTypeDef Radio_SPI_Transmit_Receive_Message(uint8_t * pTxData, uint8_t * pRxData, size_t numTransmittedReceived){
 
 	// HAL_SPI_TransmitReceive_IT(&hspi2, pTxData, pRxData, sizeof(pTxData));
 	return HAL_SPI_TransmitReceive(&hspi2, pTxData, pRxData, numTransmittedReceived, HAL_MAX_DELAY);
 }
 
-/**
- * @brief Transmit Callback Function
- */
+// TODO: Add pin for SDN and HAL variables.
+HAL_StatusTypeDef Si4464_Reset_Device()
+{
+	HAL_StatusTypeDef operation_status = HAL_OK;
+	// operation_status = HAL_GPIO_WritePin();
+	if (operation_status != HAL_OK) goto error;
+	HAL_Delay(1);
+	// operation_status = HAL_GPIO_WritePin();
+	if (operation_status != HAL_OK) goto error;
+
+error:
+	return operation_status;
+}
+
 void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef * hspi2){
 	// Handle Transmit Callback
 	// Thoughts: Not really sure what a transmit callback would be useful for. -GD
 	return;
 }
 
-/**
- * @brief Receive Callback Function
- */
 void HAL_SPI_RxCpltCallback(SPI_HandleTypeDef * hspi2){
 	// Handle Receive Callback
 	// Thoughts: The data should now be moved into a RTOS queue? -GD
 	return;
 }
 
-/**
- * @brief Transmit/Receive Callback Function
- */
 void HAL_SPI_TxRxCpltCallback (SPI_HandleTypeDef * hspi2){
 	// Handle Transmit Receive Callback
 	// Thoughts: Similar to the regular receive the data can be moved into a RTOS Queue? - GD
