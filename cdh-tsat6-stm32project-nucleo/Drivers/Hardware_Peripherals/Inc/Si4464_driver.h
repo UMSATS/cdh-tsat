c59e36d524964c06aae27efbcf2dbe0c709cace2
Nikolaus J. Reichert <nikolaus.reichert@umsats.ca>
2023-01-18 17:33:25 -0600

    finish command-sending routine.

--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/Si4464_driver.h
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/Si4464_driver.h
@@ -2,13 +2,18 @@
  * radio_spi_Si4464.h
  *
  *  Created on: Jan. 3, 2023
- *      Author: Graham Driver
+ *      Authors:
+ *      Graham Driver
+ *      Nikolaus J. Reichert <nikolaus.reichert@umsats.ca>
  */
 
 #ifndef HARDWARE_PERIPHERALS_INC_SI4464_DRIVER_H_
 #define HARDWARE_PERIPHERALS_INC_SI4464_DRIVER_H_
 
+#include <stdbool.h>
+
 #include "stm32l4xx_hal.h"
+#include "main.h"
 
 // The SPI bus used for the radio module
 extern SPI_HandleTypeDef hspi2;
@@ -72,10 +77,8 @@ HAL_StatusTypeDef Radio_SPI_Transmit_Receive_Message(uint8_t * pTxData, uint8_t 
 
 /**
  * @brief Reset the Radio via its SDN Pin.
- *
- * @returns the status code returned by the HAL.
  */
-HAL_StatusTypeDef Si4464_Reset_Device();
+void Si4464_Reset_Device();
 
 /**
  * @brief Send a command to the Radio module.
@@ -97,7 +100,13 @@ HAL_StatusTypeDef Si4464_Send_Command(uint8_t command_byte, uint8_t *argument_by
  */
 HAL_StatusTypeDef Si4464_Send_Command_Ignore_Received(uint8_t command_byte, uint8_t *argument_bytes, size_t arg_size);
 
-
+/**
+ * @brief Check if the Radio Module is ready to return requested data.
+ *
+ * @returns true if it is ready (received byte was 0xFF)
+ * @returns false if not (received byte most likely will be 0x00).
+ */
+bool Si4464_Get_CTS();
 
 #endif /* INC_SI446X_RADIO_SPI_SI4464_H_ */
 
--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/Si4464_driver.c
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/Si4464_driver.c
@@ -9,6 +9,7 @@
 #include <stdio.h>
 #include <Si4464_driver.h>
 
+#include "Si4464_driver.h"
 
 HAL_StatusTypeDef Radio_SPI_Transmit_Message(uint8_t * pData, size_t numToSend){
 	// TODO: Check which form of SPI Transfer function we should use: Blocking, IT, or DMA.
@@ -29,6 +30,19 @@ HAL_StatusTypeDef Radio_SPI_Transmit_Receive_Message(uint8_t * pTxData, uint8_t 
 	return HAL_SPI_TransmitReceive(&hspi2, pTxData, pRxData, numTransmittedReceived, HAL_MAX_DELAY);
 }
 
+bool Si4464_Get_CTS() {
+	// TODO: Make command a define somewhere, alongside all the other commands. -NJR
+	uint8_t command = 0x44;
+	uint8_t response = 0x00;
+
+	// HACK ALERT!!!!!
+	// This does not use the HAL_StatusTypeDef for returning! maybe figure out a returnable struct for error handling? -NJR
+	Radio_SPI_Transmit_Message(&command, 1);
+	Radio_SPI_Receive_Message(&response, 1);
+
+	return (response == 0xFF);
+}
+
 HAL_StatusTypeDef Si4464_Send_Command(uint8_t command_byte, uint8_t *argument_bytes, size_t arg_size, uint8_t *returned_bytes, size_t return_size)
 {
 	// HACK ALERT!!!!!!!!!!!!!!
@@ -44,11 +58,13 @@ HAL_StatusTypeDef Si4464_Send_Command(uint8_t command_byte, uint8_t *argument_by
 	operation_status = Radio_SPI_Transmit_Message(argument_bytes, arg_size);
 	if (operation_status != HAL_OK) goto error;
 
-	// TODO: Look into moving this into a function? -NJR
-	do {
-	operation_status
+	// TODO: Get a timeout here? -NJR
+	while (!Si4464_Get_CTS()) {
+		// Nothing.
+	}
 
-	} while (is_ready = false;)
+	operation_status = Radio_SPI_Receive_Message(returned_bytes, return_size);
+	if (operation_status != HAL_OK) goto error;
 
 error:
 	HAL_GPIO_WritePin(UHF_nCS_GPIO_Port, UHF_nCS_Pin, SET);
