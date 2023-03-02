ecb79e1821bae7e9b49c49907347862035884c77
Syed Abraham Ahmed <ahmeda16@myumanitoba.ca>
2023-03-02 15:53:24 -0600

    Correction for File Renaming
    Renamed and Repositioned header and source files

--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/camera_driver.h
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/camera_driver.h
@@ -8,9 +8,53 @@
  *
  * CREATED ON: March 2, 2023
  */
-
 #ifndef HARDWARE_PERIPHERALS_INC_CAMERA_DRIVER_H_
 #define HARDWARE_PERIPHERALS_INC_CAMERA_DRIVER_H_
+
+/************************************************************************************************
+/Include Directives
+************************************************************************************************/
+#include "stm32l4xx_hal.h"
+
+#include <stdint.h>
+
+/************************************************************************************************
+/Public Define Directives
+************************************************************************************************/
+
+//Initial Definitions
+#define piCAM_UART 			huart4
+#define piCAM_UART_DELAY	HAL_MAX_DELAY
+
+//Pin Definitions
+#define piCAM_ON_GPIO		GPIOA
+#define piCAM_ON_PIN 		GPIO_PIN_10
+
+#define piCAM_RX_GPIO		GPIOC
+#define piCAM_RX_PIN		GPIO_PIN_11
+
+#define piCAM_TX_GPIO		GPIOA
+#define piCAM_TX_PIN		GPIO_PIN_0
+
+#define piCAM_FSH_GPIO		GPIOA
+#define piCAM_FSH_PIN		GPIO_PIN_9
+
+/************************************************************************************************
+/Public Type Definitions
+************************************************************************************************/
+typedef enum
+{
+    piCAM_HAL_OK            = HAL_OK,      //0x00
+	piCAM_HAL_ERRO          = HAL_ERROR,   //0x01
+	piCAM_HAL_BUSY          = HAL_BUSY,    //0x02
+	piCAM_HAL_TIMEOUT       = HAL_TIMEOUT, //0x03
+	piCAM_READY            	= 0x04
+
+} piCAM_StatusTypeDef;
+
+/************************************************************************************************
+/Public Driver Functions
+************************************************************************************************/
 
 
 
--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/camera_driver.c
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/camera_driver.c
@@ -1,61 +1,19 @@
 /*
- * FILENAME: camera_driver.h
+ * FILENAME: camera_driver.c
  *
- * DESCRIPTION: STM32L4 driver header file for the piCAM Space Grade Camera from Skyfox Labs.
+ * DESCRIPTION: STM32L4 driver source file for the piCAM Space Grade Camera from Skyfox Labs.
  *
  * AUTHORS:
  *  - Syed Abraham Ahmed (syed.ahmd@umsats.ca)
  *
  * CREATED ON: March 2, 2023
  */
-#ifndef HARDWARE_PERIPHERALS_INC_CAMERA_DRIVER_H_
-#define HARDWARE_PERIPHERALS_INC_CAMERA_DRIVER_H_
 
 /************************************************************************************************
 /Include Directives
 ************************************************************************************************/
-#include "stm32l4xx_hal.h"
-
 #include <stdint.h>
 
-/************************************************************************************************
-/Public Define Directives
-************************************************************************************************/
+#include "stm32l4xx_hal.h"
+#include "camera_driver.h"
 
-//Initial Definitions
-#define piCAM_UART 			huart4
-#define piCAM_UART_DELAY	HAL_MAX_DELAY
-
-//Pin Definitions
-#define piCAM_ON_GPIO		GPIOA
-#define piCAM_ON_PIN 		GPIO_PIN_10
-
-#define piCAM_RX_GPIO		GPIOC
-#define piCAM_RX_PIN		GPIO_PIN_11
-
-#define piCAM_TX_GPIO		GPIOA
-#define piCAM_TX_PIN		GPIO_PIN_0
-
-#define piCAM_FSH_GPIO		GPIOA
-#define piCAM_FSH_PIN		GPIO_PIN_9
-
-/************************************************************************************************
-/Public Type Definitions
-************************************************************************************************/
-typedef enum
-{
-    piCAM_HAL_OK            = HAL_OK,      //0x00
-	piCAM_HAL_ERRO          = HAL_ERROR,   //0x01
-	piCAM_HAL_BUSY          = HAL_BUSY,    //0x02
-	piCAM_HAL_TIMEOUT       = HAL_TIMEOUT, //0x03
-	piCAM_READY            	= 0x04
-
-} piCAM_StatusTypeDef;
-
-/************************************************************************************************
-/Public Driver Functions
-************************************************************************************************/
-
-
-
-#endif /* HARDWARE_PERIPHERALS_INC_CAMERA_DRIVER_H_ */
