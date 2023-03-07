a9c0e8a4baf869e72cc4a2b8b4612418ac89a463
Gabriel J. Young <gabriel.young@umsats.ca>
2023-03-07 16:23:56 -0600

    MRAM test suite no longer uses heap-allocated buffers

--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Test/Src/AS3001204_driver_test.c
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Test/Src/AS3001204_driver_test.c
@@ -23,14 +23,16 @@
 // Defining test data
 //###############################################################################################
 
-static uint8_t STATUS_REG_DEFAULT = 0x00;
-static uint8_t STATUS_REG_TEST = 0x60;
+static const uint8_t STATUS_REG_DEFAULT = 0x00;
+static const uint8_t STATUS_REG_TEST    = 0x60;
 
-static uint8_t CONFIG_REGS_DEFAULT[AS3001204_CONFIG_REGS_LENGTH] = {0x00, 0x00, 0x60, 0x05};
-static uint8_t CONFIG_REGS_TEST[AS3001204_CONFIG_REGS_LENGTH] = {0x05, 0x0f, 0x73, 0x06};
+static const uint8_t CONFIG_REGS_DEFAULT[AS3001204_CONFIG_REGS_LENGTH] = {0x00, 0x00, 0x60, 0x05};
+static const uint8_t CONFIG_REGS_TEST   [AS3001204_CONFIG_REGS_LENGTH] = {0x05, 0x0f, 0x73, 0x06};
 
-static uint8_t AAP_REG_DEFAULT = 0x00;
-static uint8_t AAP_REG_TEST = 0xff;
+static const uint8_t DEVICE_ID[AS3001204_DEVICE_ID_LENGTH] = {0xE6, 0x01, 0x01, 0x02};
+
+static const uint8_t AAP_REG_DEFAULT = 0x00;
+static const uint8_t AAP_REG_TEST    = 0xff;
 
 static const char *SAMPLE_DATA = "All human beings are born free and equal in dignity and rights. \
 They are endowed with reason and conscience and should act towards one another in a spirit of brotherhood.";
@@ -64,7 +66,7 @@ error:
 unsigned int AS3001204_Test_RW_Config_Registers() {
 
     HAL_StatusTypeDef isError;
-    uint8_t *p_buffer = malloc(AS3001204_CONFIG_REGS_LENGTH);
+    uint8_t p_buffer[AS3001204_CONFIG_REGS_LENGTH];
 
     isError = AS3001204_Write_Config_Registers(CONFIG_REGS_TEST);
     if (isError != HAL_OK) goto error;
@@ -75,15 +77,14 @@ unsigned int AS3001204_Test_RW_Config_Registers() {
     if (memcmp(p_buffer, CONFIG_REGS_TEST, AS3001204_CONFIG_REGS_LENGTH) == 0) return 0;
 
 error:
-	free(p_buffer);
     return 1;
     
 }
 
 
 unsigned int AS3001204_Test_RW_Augmented_Array_Protection_Register() {
-	
-	HAL_StatusTypeDef isError; 
+    
+    HAL_StatusTypeDef isError; 
     uint8_t p_buffer = 0x00;
 
     isError = AS3001204_Write_Augmented_Array_Protection_Register(&AAP_REG_TEST);
@@ -96,7 +97,7 @@ unsigned int AS3001204_Test_RW_Augmented_Array_Protection_Register() {
 
 error:
     return 1;
-	
+    
 }
 
 
@@ -105,125 +106,101 @@ error:
 //###############################################################################################
 
 unsigned int AS3001204_Test_RW_Memory() {
-	
-	HAL_StatusTypeDef isError;
-	uint8_t *p_buffer = malloc(sizeof SAMPLE_DATA);
-	
-	isError = AS3001204_Write_Memory((uint8_t *) SAMPLE_DATA, 0xabba, sizeof SAMPLE_DATA);
-	if (isError != HAL_OK) goto error;
-	
-	isError = AS3001204_Read_Memory(p_buffer, 0xabba, sizeof SAMPLE_DATA);
-	if (isError != HAL_OK) goto error;
-	
-	if (memcmp(p_buffer, SAMPLE_DATA, strlen(SAMPLE_DATA)) == 0) return 0;
-	
+    
+    HAL_StatusTypeDef isError;
+    uint8_t p_buffer[strlen(SAMPLE_DATA)];
+    
+    isError = AS3001204_Write_Memory((uint8_t *) SAMPLE_DATA, 0xabba, sizeof SAMPLE_DATA);
+    if (isError != HAL_OK) goto error;
+    
+    isError = AS3001204_Read_Memory(p_buffer, 0xabba, sizeof SAMPLE_DATA);
+    if (isError != HAL_OK) goto error;
+    
+    if (memcmp(p_buffer, SAMPLE_DATA, strlen(SAMPLE_DATA)) == 0) return 0;
+    
 error:
-	free(p_buffer);
     return 1;
 }
 
 
 unsigned int AS3001204_Test_RW_Augmented_Storage() {
-	
-	HAL_StatusTypeDef isError;
-	uint8_t *p_buffer = malloc(sizeof SAMPLE_DATA);
-	
-	isError = AS3001204_Write_Augmented_Storage((uint8_t *) &SAMPLE_DATA, 0x00, sizeof SAMPLE_DATA);
-	if (isError != HAL_OK) goto error;
-	
-	isError = AS3001204_Read_Augmented_Storage(p_buffer, 0x00, sizeof SAMPLE_DATA);
-	if (isError != HAL_OK) goto error;
-	
-	if (memcmp(p_buffer, SAMPLE_DATA, strlen(SAMPLE_DATA) == 0)) return 0;
-	
+    
+    HAL_StatusTypeDef isError;
+    uint8_t p_buffer[strlen(SAMPLE_DATA)];
+    
+    isError = AS3001204_Write_Augmented_Storage((uint8_t *) &SAMPLE_DATA, 0x00, sizeof SAMPLE_DATA);
+    if (isError != HAL_OK) goto error;
+    
+    isError = AS3001204_Read_Augmented_Storage(p_buffer, 0x00, sizeof SAMPLE_DATA);
+    if (isError != HAL_OK) goto error;
+    
+    return strcmp(p_buffer, SAMPLE_DATA, strlen(SAMPLE_DATA) == 0);
+    
 error:
-	free(p_buffer);
     return 1;
-	
+    
 }
 
 unsigned int AS3001204_Test_Read_ID_Registers() {
 
-	HAL_StatusTypeDef isError;
-	// uint8_t *devIDBuffer = malloc(AS3001204_DEVICE_ID_LENGTH);
-	// uint8_t *uniIDBuffer = malloc(AS3001204_UNIQUE_ID_LENGTH);
-	uint8_t devIDBuffer[AS3001204_DEVICE_ID_LENGTH];
-	uint8_t uniIDBuffer[AS3001204_UNIQUE_ID_LENGTH];
+    HAL_StatusTypeDef isError;
+    uint8_t devIDBuffer[AS3001204_DEVICE_ID_LENGTH];
+    uint8_t uniIDBuffer[AS3001204_UNIQUE_ID_LENGTH];
 
-	isError = AS3001204_Read_Device_ID(devIDBuffer);
-	if (isError != HAL_OK) goto error;
+    isError = AS3001204_Read_Device_ID(devIDBuffer);
+    if (isError != HAL_OK) goto error;
 
-	isError = AS3001204_Read_Unique_ID(uniIDBuffer);
-	if (isError != HAL_OK) goto error;
+    isError = AS3001204_Read_Unique_ID(uniIDBuffer);
+    if (isError != HAL_OK) goto error;
 
-	uint8_t deviceID[AS3001204_DEVICE_ID_LENGTH] = {0xE6, 0x01, 0x01, 0x02};
-
-	int result = memcmp(devIDBuffer, &deviceID, AS3001204_DEVICE_ID_LENGTH);
-
-	return result != 0;
+    return devIDBuffer != DEVICE_ID;
 
 error:
-	return 1;
+    return 1;
 }
 
+// This function will have to be made a bit more imaginative, i.e. try writing something
+// during write disable and make sure it didn't happen. That may ruin the hidden implementation 
+// of setting write enable in the write functions, though. -GY
 unsigned int AS3001204_Test_Write_Disable() {
-	
-	HAL_StatusTypeDef isError;
-	
-	isError = AS3001204_Write_Disable();
-	if (isError != HAL_OK) goto error;
-	return 0;
 
-error:
-	printf("Failed to write disable\n");
-	return 1;
+    HAL_StatusTypeDef isError;
+    
+    isError = AS3001204_Write_Disable();
+    
+    return (isError != HAL_OK);
 }
 
-unsigned int AS3001204_Test_Software_Reset_Enable() {
-	
-	HAL_StatusTypeDef isError;
-	
-	isError = AS3001204_Software_Reset_Enable();
-	if (isError != HAL_OK) goto error;
-	return 0;
-
-error:
-	printf("Failed to enable software reset\n");
-	return 1;
-}
-
-unsigned int AS3001204_Test_Software_Reset() {
-	
-}
 
 //###############################################################################################
 // Complete test suite routine
 //###############################################################################################
 
-
 int AS3001204_Test_Mram_Driver() {
-	int numFailed = 0;
+    int numFailed = 0;
 
-	HAL_GPIO_WritePin(AS3001204_nWP_GPIO, AS3001204_nWP_PIN, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(AS3001204_nWP_GPIO, AS3001204_nWP_PIN, GPIO_PIN_SET);
 
-//	numFailed += AS3001204_Test_Read_ID_Registers();
+    // numFailed += AS3001204_Test_Read_ID_Registers();
 
-	numFailed += AS3001204_Test_RW_Status_Register();
-//	numFailed += AS3001204_Test_RW_Config_Registers();
-//	numFailed += AS3001204_Test_RW_Augmented_Array_Protection_Register();
-//	numFailed += AS3001204_Test_RW_Memory();
-//	numFailed += AS3001204_Test_RW_Augmented_Storage();
-//	numFailed += AS3001204_Test_Write_Disable();
+    numFailed += AS3001204_Test_RW_Status_Register();
+    // numFailed += AS3001204_Test_RW_Config_Registers();
+    // numFailed += AS3001204_Test_RW_Augmented_Array_Protection_Register();
+    // numFailed += AS3001204_Test_RW_Memory();
+    // numFailed += AS3001204_Test_RW_Augmented_Storage();
 
-	// Functions not tested in the above (note WREN is indirectly tested)
+    // This function sucks and won't be included right now, see note above
+    // numFailed += AS3001204_Test_Write_Disable();
 
-//	AS3001204_Enter_Hibernate();
-//	AS3001204_Enter_Deep_Power_Down();
-//	AS3001204_Exit_Deep_Power_Down();
-//	AS3001204_Software_Reset_Enable();
-//	AS3001204_Software_Reset();
 
-	return numFailed;
+    // Note: no tests currently for any of the following driver functions:
+    //	AS3001204_Enter_Hibernate();
+    //	AS3001204_Enter_Deep_Power_Down();
+    //	AS3001204_Exit_Deep_Power_Down();
+    //	AS3001204_Software_Reset_Enable();
+    //	AS3001204_Software_Reset();
+
+    return numFailed;
 }
 
 
