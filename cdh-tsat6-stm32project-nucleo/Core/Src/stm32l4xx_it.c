025b47fcc2a9b10016e0b8a16daac94bb10be6fe
Atlas2k <amouri2000@hotmail.com>
2023-05-25 21:08:53 -0500

    Add UART4 global interrupt

--- cdh-tsat6-stm32project-nucleo/Core/Src/stm32l4xx_it.c
+++ cdh-tsat6-stm32project-nucleo/Core/Src/stm32l4xx_it.c
@@ -56,6 +56,7 @@
 
 /* External variables --------------------------------------------------------*/
 extern DMA_HandleTypeDef hdma_uart4_rx;
+extern UART_HandleTypeDef huart4;
 /* USER CODE BEGIN EV */
 
 /* USER CODE END EV */
@@ -197,6 +198,20 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32l4xx.s).                    */
 /******************************************************************************/
+
+/**
+  * @brief This function handles UART4 global interrupt.
+  */
+void UART4_IRQHandler(void)
+{
+  /* USER CODE BEGIN UART4_IRQn 0 */
+
+  /* USER CODE END UART4_IRQn 0 */
+  HAL_UART_IRQHandler(&huart4);
+  /* USER CODE BEGIN UART4_IRQn 1 */
+
+  /* USER CODE END UART4_IRQn 1 */
+}
 
 /**
   * @brief This function handles DMA2 channel5 global interrupt.
