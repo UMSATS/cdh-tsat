db1c72e41bb0933c7b72d179f30bd6fb535435cf
Graham Driver <drivergraham1@gmail.com>
2023-11-13 12:28:08 -0600

    Initial Commit
    - Added SP-L2 Driver & Header Files

---
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/SP-L2_driver.h
@@ -0,0 +1,20 @@
+/*
+ * FILENAME: SP-L2_driver.h
+ *
+ * DESCRIPTION: STM32L4 header driver file for the SP-L2 UHF radio transceiver
+ *
+ * AUTHORS:
+ *  - Graham Driver (graham.driver@umsats.ca)
+ *
+ * CREATED ON: November 13th 2023
+ */
+
+#ifndef HARDWARE_PERIPHERALS_INC_SP_L2_DRIVER_H_
+#define HARDWARE_PERIPHERALS_INC_SP_L2_DRIVER_H_
+//###############################################################################################
+// Include Directives
+//###############################################################################################
+#include "stm32l4xx_hal.h"
+
+
+#endif /* HARDWARE_PERIPHERALS_INC_SP_L2_DRIVER_H_ */
---
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/SP-L2_driver.c
@@ -0,0 +1,11 @@
+/*
+ * FILENAME: SP-L2_driver.c
+ *
+ * DESCRIPTION: STM32L4 driver file for the SP-L2 UHF radio transceiver
+ *
+ * AUTHORS:
+ *  - Graham Driver (graham.driver@umsats.ca)
+ *
+ * CREATED ON: November 13th 2023
+ */
+
