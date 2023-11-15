df153b7a7f8673a01a8713b17eb527833b9431e5
Graham Driver <51301220+GrahamDrive@users.noreply.github.com>
2023-11-15 16:30:13 -0600

    Framework for driver
    - Added some basic function prototypes
    - Wrote basic wrapper for SPI

--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/SP-L2_driver.h
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Inc/SP-L2_driver.h
@@ -15,6 +15,91 @@
 // Include Directives
 //###############################################################################################
 #include "stm32l4xx_hal.h"
+#include "main.h"
+
+typedef enum
+{
+	SPL2_HAL_OK                     = HAL_OK,      //0x00
+	SPL2_HAL_ERROR                  = HAL_ERROR,   //0x01
+	SPL2_HAL_BUSY                   = HAL_BUSY,    //0x02
+	SPL2_HAL_TIMEOUT                = HAL_TIMEOUT, //0x03
+} SPL2_StatusTypeDef;
+
+//###############################################################################################
+// Function Prototypes
+//###############################################################################################
+
+
+/**
+ * @brief Sends Message over SPI to the radio
+ *
+ * *Note* The Radio will use SPI 2 on the mcu.
+ */
+SPL2_StatusTypeDef SPL2_Spi_Send_Message(uint8_t * pData, size_t numToSend);
+
+
+/**
+ * @brief Receives Message over SPI to the radio
+ *
+ *
+ */
+SPL2_StatusTypeDef SPL2_Spi_Receive_Message(uint8_t * pData, size_t numToReceive);
+
+
+/**
+ * @brief Transmit & Receives Message over SPI to the radio
+ *
+ *
+ */
+SPL2_StatusTypeDef SPL2_SPI_Transmit_Receive_Message(uint8_t *pTxData, uint8_t *pRxData, size_t numTransmitReceive);
+
+
+/**
+ * @brief Writes to the S2-LP registers.
+ *
+ *
+ */
+SPL2_StatusTypeDef S2LP_Spi_Write_Registers(uint8_t address, uint8_t n_regs, uint8_t* buffer);
+
+
+/**
+ * @brief Reads the S2-LP registers.
+ *
+ *
+ */
+SPL2_StatusTypeDef S2LP_Spi_Read_Registers(uint8_t address, uint8_t n_regs, uint8_t* buffer);
+
+
+/**
+ * @brief Writes to the S2-LP TX FIFO.
+ *
+ *
+ */
+SPL2_StatusTypeDef S2LP_Spi_Write_TX_Fifo(uint8_t address, uint8_t n_regs, uint8_t* buffer);
+
+
+/**
+ * @brief Reads the S2-LP RX FIFO.
+ *
+ *
+ */
+SPL2_StatusTypeDef S2LP_Spi_Read_RX_Fifo(uint8_t address, uint8_t n_regs, uint8_t* buffer);
+
+
+/**
+ * @brief Reads the S2-LP RX FIFO.
+ *
+ *
+ */
+SPL2_StatusTypeDef S2LP_Spi_Send_Command(uint8_t address, uint8_t n_regs, uint8_t* buffer);
+
+/**
+ * @brief Shuts down SP-L2.
+ *
+ * *NOTE* Page 24 of SP-L2 documentation Describes Reset procedure
+ */
+SPL2_StatusTypeDef S2LP_Reset();
+
 
 
 #endif /* HARDWARE_PERIPHERALS_INC_SP_L2_DRIVER_H_ */
--- cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/SP-L2_driver.c
+++ cdh-tsat6-stm32project-nucleo/Drivers/Hardware_Peripherals/Src/SP-L2_driver.c
@@ -9,3 +9,19 @@
  * CREATED ON: November 13th 2023
  */
 
+#include "SP-L2_driver.h"
+
+SPL2_StatusTypeDef SPL2_Spi_Send_Message(uint8_t * pData, size_t numToSend){
+
+	return HAL_SPI_Transmit(&hspi2, pData, numToSend, HAL_MAX_DELAY);
+}
+
+SPL2_StatusTypeDef SPL2_Spi_Receive_Message(uint8_t * pData, size_t numToReceive){
+
+	return HAL_SPI_Receive_DMA(&hspi2, pData, numToReceive);
+}
+
+SPL2_StatusTypeDef SPL2_SPI_Transmit_Receive_Message(uint8_t *pTxData, uint8_t *pRxData, size_t numTransmitReceive){
+
+	return HAL_SPI_TransmitReceive(&hspi2, pTxData, pRxData, numTransmitReceive);
+}
