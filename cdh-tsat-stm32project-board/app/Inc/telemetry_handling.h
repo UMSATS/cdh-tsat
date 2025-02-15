d044944525f9080fd683e1609eac9e799f4dd452
Graham Driver <drivergraham1@gmail.com>
2025-02-15 14:02:11 -0600

    Telemetry Handling Task Ported

---
+++ cdh-tsat-stm32project-board/app/Inc/telemetry_handling.h
@@ -0,0 +1,25 @@
+/*
+ * telemetry_handling.h
+ *
+ *  Created on: Feb 15, 2025
+ *      Author: drive
+ */
+
+#ifndef INC_TELEMETRY_HANDLING_H_
+#define INC_TELEMETRY_HANDLING_H_
+
+#include "stm32l4xx_hal.h"
+#include "cmsis_os.h"
+#include "telemetry.h"
+
+extern osMessageQueueId_t telemQueueHandle;
+
+
+/**
+* @brief Function implementing the telemHandler thread.
+* @param argument: Not used
+* @retval None
+*/
+void StartTelemHandler(void *argument);
+
+#endif /* INC_TELEMETRY_HANDLING_H_ */
