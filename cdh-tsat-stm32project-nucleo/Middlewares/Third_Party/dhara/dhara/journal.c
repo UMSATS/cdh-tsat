e389471da13420b6171bb37cb2370aac67b1409c
FlyingMonkey4 <113262557+FlyingMonkey4@users.noreply.github.com>
2025-11-16 23:17:38 -0600

    Dhara library working
    I have the dhara library working but will now create useful wrapper functions to make life easier when using the library. Also will attempt to make unit test functions for the library. Another thing that needs to be fixed is the usedSparesCount that is currently only accessible in the nand.c file and cant be changed from the dharaInit function.

--- cdh-tsat-stm32project-nucleo/Middlewares/Third_Party/dhara/dhara/journal.c
+++ cdh-tsat-stm32project-nucleo/Middlewares/Third_Party/dhara/dhara/journal.c
@@ -174,7 +174,6 @@ static void clear_recovery(struct dhara_journal *j)
 /* Set up an empty journal */
 static void reset_journal(struct dhara_journal *j)
 {
-	uint8_t* temp;
 	/* We don't yet have a bad block estimate, so make a
 	 * conservative guess.
 	 */
@@ -195,7 +194,6 @@ static void reset_journal(struct dhara_journal *j)
 
 	/* Empty metadata buffer */
 	memset(j->page_buf, 0xff, 1 << j->nand->log2_page_size);
-	memset(temp, 0xff, 1 << j->nand->log2_page_size);
 }
 
 static void roll_stats(struct dhara_journal *j)
@@ -232,14 +230,6 @@ static int find_checkblock(struct dhara_journal *j,
 		const dhara_page_t p =
 			(blk << j->nand->log2_ppb) |
 			((1 << j->log2_ppc) - 1);
-		int testnumber=dhara_nand_is_bad(j->nand, blk);
-		testnumber=dhara_nand_read(j->nand, p,
-			      0, 1 << j->nand->log2_page_size,
-			      j->page_buf, err);
-		testnumber=(!(dhara_nand_is_bad(j->nand, blk) ||
-			      dhara_nand_read(j->nand, p,
-					      0, 1 << j->nand->log2_page_size,
-					      j->page_buf, err)));
 
 		if (!(dhara_nand_is_bad(j->nand, blk) ||
 		      dhara_nand_read(j->nand, p,
