0e3fc950a90546b6f1c8429d7b64675bc25b5aeb
FlyingMonkey4 <113262557+FlyingMonkey4@users.noreply.github.com>
2026-03-17 22:58:01 -0500

    Removed trim
    whole trim system isnt needed anymore

--- cdh-tsat-stm32project-nucleo/Core/Inc/StorageManager.h
+++ cdh-tsat-stm32project-nucleo/Core/Inc/StorageManager.h
@@ -183,15 +183,4 @@ int Storage_Get_SectorNode(const DataType dType, const uint8_t sType, const uint
 */
 int Storage_Read(const SectorNode* sectorNode, uint8_t* data, uint32_t dataSize);
 
-/*
- * FUNCTION: Storage_Trim
- *
- * DESCRIPTION: Trims down current storage area by replacing empty sector near the front with filled sectors.
- *
- * RETURNS:
- * 		0 on success or -1 if an error occurs.
-*/
-int Storage_Trim();
-
-
 #endif /* INC_STORAGEMANAGER_H_ */
